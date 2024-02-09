27b4bf091d676ac43d39d21810d32bb8c0a8e9e1
EstebanMarin <estebanmarin002@gmail.com>
2024-02-09 16:33:27 -0500

    working server

--- src/main/scala/com/esteban/rockjvm/Main.scala
+++ src/main/scala/com/esteban/rockjvm/Main.scala
@@ -2,26 +2,8 @@ package com.esteban
 package rockjvm
 
 import cats.*
-import cats.implicits.*
 import cats.effect.*
-import io.circe.*
-import io.circe.syntax.*
-import io.circe.parser.*
-import io.circe.generic.semiauto.*
-import org.http4s.circe.*
-import org.http4s.headers.*
-import org.http4s.*
-import org.http4s.dsl.*
-import org.http4s.dsl.impl.*
-import org.http4s.implicits.*
-import org.http4s.server.*
-import org.http4s.ember.server.EmberServerBuilder
-import java.util.UUID
-import org.http4s.dsl.io.*
-import org.typelevel.ci.CIString
-import com.comcast.ip4s.{ Host as ComHost, Port as ComPort }
-import com.esteban.rockjvm.config.Config
-import fs2.compression.DeflateParams.Level.FIVE
+import com.esteban.rockjvm.http.com.example.http4s.ember.ServerService
 
 /*
  1 - add health endpoint
@@ -31,13 +13,5 @@ import fs2.compression.DeflateParams.Level.FIVE
  */
 
 object MainApp extends IOApp.Simple:
-  // def healthEndpoint[F[_]: Monad] = HttpRoutes.of[F]:
-  //   val dsl = Http4sDsl[F]
-  //   import dsl.*
-
-  //   case GET -> Root / "health" =>
-  //     Ok("I'm alive!")
-
-  // val httpApp = healthEndpoint[IO].orNotFound
   def run: IO[Unit] =
-    IO.println("Hello, world!")
+    ServerService.make[IO].server.use(_ => IO.never)
--- src/main/scala/com/esteban/rockjvm/http/Server.scala
+++ src/main/scala/com/esteban/rockjvm/http/Server.scala
@@ -2,21 +2,12 @@ package com.esteban.rockjvm.http
 
 package com.example.http4s.ember
 
-import _root_.io.circe.*
 import _root_.org.http4s.ember.server.EmberServerBuilder
 import cats.effect.*
-import cats.syntax.all.*
-import _root_.com.comcast.ip4s.*
-// import com.comcast.ip4s.{ Host as ComHost, Port as ComPort }
-// import com.
-
 import fs2.*
 import org.http4s.*
-import org.http4s.circe.*
 import org.http4s.dsl.Http4sDsl
 import org.http4s.implicits.*
-import org.http4s.server.websocket.WebSocketBuilder2
-import org.http4s.websocket.WebSocketFrame
 import org.http4s.server.Server
 
 trait ServerService[F[_]]:
@@ -29,8 +20,8 @@ object ServerService:
     val dsl = Http4sDsl[F]
     import dsl.*
     val httpApp = HttpRoutes.of[F]:
-      case GET -> Root / "hello" =>
-        Ok("Hello, world!")
+      case GET -> Root / "ping" =>
+        Ok("pong")
     val server: Resource[F, Server] = EmberServerBuilder
       .default[F]
       .withHttpApp(httpApp.orNotFound)
