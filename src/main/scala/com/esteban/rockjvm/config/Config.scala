c8764f654435cf367b2f75d8e3da66ed7bffad9b
EstebanMarin <estebanmarin002@gmail.com>
2024-02-09 18:36:04 -0500

    compiling

--- src/main/scala/com/esteban/rockjvm/config/Config.scala
+++ src/main/scala/com/esteban/rockjvm/config/Config.scala
@@ -14,13 +14,15 @@ trait Config[F[_]: MonadThrow]:
 object Config:
   given ConfigDecoder[String, Port] =
     ???
+
   given ConfigDecoder[String, Host] =
     ???
+
   def make[F[_]: MonadThrow]: Config[F] = new Config[F]:
     val databaseConfig: ConfigValue[Effect, ServerConfig] =
       (
         env("SERVER_PORT").as[Port].default(Port.fromInt(8080).get),
-        env("SERVER_HOST").as[Host],
+        env("SERVER_HOST").as[Host].default(Host.fromString("localhost").get),
       ).parMapN(ServerConfig.apply(_, _))
 
     def server: F[(Port, Host)] =
