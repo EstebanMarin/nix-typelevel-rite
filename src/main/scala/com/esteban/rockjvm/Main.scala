9457b64ec605d80eb663f4aa0b72cdfc2f9fedf7
EstebanMarin <estebanmarin002@gmail.com>
2024-02-10 18:05:28 -0500

    cleanup

--- src/main/scala/com/esteban/rockjvm/Main.scala
+++ src/main/scala/com/esteban/rockjvm/Main.scala
@@ -9,22 +9,22 @@ import com.esteban.rockjvm.http.com.example.http4s.ember.HttpService
 import com.esteban.rockjvm.database.JobRepository
 import com.esteban.rockjvm.model.ServerConfig
 import fs2.Stream
-// import fs2.io.file.{ Files, Path }
-
-// Create readiness prove that test DB connection is working
 
 object MainApp extends IOApp.Simple:
   val configStream: Stream[IO, ServerConfig] =
     Stream.bracket(Config.make[IO].serviceConfig)(_ => IO.unit)
   val dbStream: Stream[IO, JobRepository[IO]] =
     Stream.bracket(IO(JobRepository.make[IO]))(_ => IO.unit)
-  val serverStream = Stream.bracket(IO(HttpService.make[IO]))(_ => IO.unit)
+  def serverStream(config: ServerConfig) =
+    Stream.bracket(IO(HttpService.make[IO](config).server.useForever))(_ => IO.unit)
 
   val appScafolding =
     configStream.map(identity).compile.drain
 
   val test =
-    for conf: ServerConfig <- configStream
+    for
+      conf: ServerConfig <- configStream
+      server <- serverStream(conf)
     yield ()
   def run: IO[Unit] =
     for
@@ -34,24 +34,3 @@ object MainApp extends IOApp.Simple:
         IO.println(s"${jobRepo}") *>
           HttpService.make[IO](config).server.use(_ => IO.never)
     yield server
-  // configStream.flatMap((x: Config[IO]) => x.serviceConfig.flatMap() )
-  // configStream
-  //   .map(config => config.serviceConfig.flatMap(x => dbStream.zipAll(serverStream)))
-  //   .compile
-  //   .drain
-
-  // for
-  //   config: Config[IO] <- configStream
-  //   // test <- dbStream.zip(serverStream).flatMap((db, server) => db.readinessPro
-  //   test <- Stream.eval(
-  //     dbStream.zip(serverStream).flatMap((db, server) => Stream(db.readinessProve))
-  //   )
-  // yield test
-  // configStream
-  //   .zip(dbStream)
-  //   .zip(serverStream)
-  //   .map { case ((config, jobRepo), server) =>
-  //     server.server.use(_ => IO.never)
-  //   }
-  //   .compile
-  //   .drain
