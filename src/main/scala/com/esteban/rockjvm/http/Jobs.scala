7f8ef8bb50afd68aa63be220009b4c94faaa8ab2
EstebanMarin <estebanmarin002@gmail.com>
2024-02-09 20:19:28 -0500

    compiling

--- src/main/scala/com/esteban/rockjvm/http/Jobs.scala
+++ src/main/scala/com/esteban/rockjvm/http/Jobs.scala
@@ -1,13 +1,21 @@
 package com.esteban.rockjvm.http
 
+import cats.*
+import cats.implicits.*
 import org.http4s.dsl.Http4sDsl
 import org.http4s.HttpRoutes
 import org.http4s.server.Router
 import cats.effect.kernel.Async
 
-class JobsRoutes[F[_]: Async] extends Http4sDsl[F]:
-  val serviceRoutes = HttpRoutes.of[F]:
-    case GET -> Root / "service" =>
-      Ok("service")
+class JobsRoutes[F[_]: Async] private extends Http4sDsl[F]:
+  private val allJobs = HttpRoutes.of[F]:
+    case POST -> Root =>
+      Ok("TODO")
 
-  val serviceRouter = Router("/jobs" -> serviceRoutes)
+  private val jobById = HttpRoutes.of[F]:
+    case GET -> Root / UUIDVar(id) =>
+      Ok(s"TODO: get job by id $id")
+
+  private val allRoute = jobById <+> allJobs
+
+  val serviceRouter = Router("/jobs" -> allRoute)
