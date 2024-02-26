e7af8192aa8cfb103ca83112fc2a402dea23607f
EstebanMarin <estebanmarin002@gmail.com>
2024-02-25 19:29:42 -0500

    Add getJobById method to JobRepository

--- src/main/scala/com/esteban/rockjvm/database/Repository.scala
+++ src/main/scala/com/esteban/rockjvm/database/Repository.scala
@@ -8,10 +8,12 @@ import doobie.implicits.*
 import doobie.util.transactor.Transactor
 
 import com.esteban.rockjvm.model.db.Job.*
+import java.util.UUID
 
 trait JobRepository[F[_]]:
   def readinessProve: F[String]
   def allJobs: F[List[JobInfo]]
+  def getJobById(id: UUID): F[Option[JobInfo]]
 
 object JobRepository:
   def make[F[_]: Async]: JobRepository[F] =
@@ -29,3 +31,24 @@ object JobRepository:
 
       def allJobs: F[List[JobInfo]] =
         sql"select * from job_info".query[JobInfo].to[List].transact(xa)
+
+      given Meta[UUID] = ???
+      // {
+      //   Some(UUID.fromString())
+      //   UUID.fromString match
+      //     // case Some(uuid) =>         Meta[String].timap()(uuid.toString)
+      //     case Some(uuid) =>         ???
+      //     case None => throw new Exception("UUID parsing failed")
+      //   }
+
+      // Meta[String].timap()(_.toString)
+      // }
+      def getJobById(id: UUID): F[Option[JobInfo]] =
+        println(s"select * from job_info where id = '${id.toString()}';")
+        // println(sql"select * from job_info where id = '${id.toString()};")
+        // sql"select * from job_info where id = ${id.toString()}"
+        sql"select * from job_info where id = $id"
+          // sql"select * from job_info where id = 'd290f1ee-6c54-4b01-90e6-d701748f0851'"
+          .query[JobInfo]
+          .option
+          .transact(xa)
--- src/main/scala/com/esteban/rockjvm/http/Jobs.scala
+++ src/main/scala/com/esteban/rockjvm/http/Jobs.scala
@@ -1,22 +1,26 @@
 package com.esteban.rockjvm.http
 
 import _root_.com.esteban.rockjvm.database.JobRepository
+import _root_.com.esteban.rockjvm.model.db.Job.*
 import cats.*
 import cats.effect.kernel.Async
 import cats.implicits.*
-import org.http4s.HttpRoutes
+import io.circe.Codec
+import io.circe.generic.semiauto.deriveCodec
+import org.http4s.*
+import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
+import org.http4s.circe.*
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
-import _root_.com.esteban.rockjvm.model.db.Job.*
-import io.circe.Codec
-import org.http4s.EntityEncoder
-import io.circe.generic.semiauto.deriveCodec
-import org.http4s.circe.*
-class JobsRoutes[F[_]: Async: JobRepository] extends Http4sDsl[F]:
+
+class JobsRoutes[F[_]: Async: JobRepository: MonadThrow] extends Http4sDsl[F]:
   given Codec[JobInfo] =
     deriveCodec[JobInfo]
   given EntityEncoder[F, List[JobInfo]] =
     jsonEncoderOf[F, List[JobInfo]]
+
+  given EntityEncoder[F, Option[JobInfo]] =
+    jsonEncoderOf[F, Option[JobInfo]]
 
   private val allJobs = HttpRoutes.of[F]:
     case GET -> Root / "get-all" =>
@@ -24,7 +28,10 @@ class JobsRoutes[F[_]: Async: JobRepository] extends Http4sDsl[F]:
 
   private val jobById = HttpRoutes.of[F]:
     case GET -> Root / UUIDVar(id) =>
-      Ok("TODO")
+      Ok(for repo <- summon[JobRepository[F]].getJobById(id) yield repo match
+        case Some(job) => job
+        case None => throw new Exception(s"Job with id $id not found")
+      )
 
   val allRoutes = jobById <+> allJobs
 
